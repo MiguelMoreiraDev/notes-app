bf84e96702a598125f2f22fc742038957cd8be5f
Miguel Henrique Moreira <contatomigueldev@gmail.com>
2024-07-09 17:16:28 -0300

    update

--- src/pages/LoginPages/LoginForm/Index.jsx
+++ src/pages/LoginPages/LoginForm/Index.jsx
@@ -9,53 +9,6 @@ import { ArrowRight } from "lucide-react";
 import { useForm } from "../../../hooks/useForm/Index";
 
 export const LoginForm = () => {
-<<<<<<< HEAD
-  return (
-    <S.Container className="animeRight">
-      <S.Content className="content">
-        <S.TitleContainer>
-          <Title
-            variant="Title"
-            size="text_2xl"
-            weight={800}
-            color="black"
-            typeColor="neutral"
-          >
-            Let&apos;s Login
-          </Title>
-          <Title
-            variant="Subtitle"
-            size="text_base"
-            weight={500}
-            color="dark_gray"
-            typeColor="neutral"
-          >
-            And notes your day
-          </Title>
-        </S.TitleContainer>
-        <S.FormContainer>
-          <Input
-            label="Email Address"
-            placeholder={"Example: email@gmail.com"}
-          />
-          <Input label="Password" placeholder={"******"} />
-        </S.FormContainer>
-        <Link link={"/login/lost-password"}>Forgot Password</Link>
-        <S.ButtonContainer>
-          <PrimaryButton variant="primaryIconEnd" endIcon={<ArrowRight />}>
-            Login
-          </PrimaryButton>
-          <S.Detail>
-            <p>or</p>
-          </S.Detail>
-          <Link link={"/login/create-account"} underline={false}>
-            Don´t have any account? Register here
-          </Link>
-        </S.ButtonContainer>
-      </S.Content>
-    </S.Container>
-  );
-=======
     const email = useForm("email");
     const password = useForm();
 
@@ -114,5 +67,4 @@ export const LoginForm = () => {
             </S.Content>
         </S.Container>
     );
->>>>>>> 5866b071fd3d1e45f58a5ae074d30c350219b73f
 };
